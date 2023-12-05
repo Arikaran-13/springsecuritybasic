7a2cd4c40faed518c5daefaa64be6b23adb1b086
Arikaran Sivasankar <arikaran.sivasankar@vanderlande.com>
2023-12-05 19:47:30 +0530

    Initial commit

---
+++ src/main/java/com/example/springsecuritybasic/controller/CardsController.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardsController {
+
+    @GetMapping("/cards")
+    public String getCardDetails(){
+        return "Returns card detail from DB";
+    }
+}
---
+++ src/main/java/com/example/springsecuritybasic/controller/NoticeController.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticeController {
+
+    @GetMapping("/notice")
+    public String getNotices(){
+        return "displaying notices from db";
+    }
+}
